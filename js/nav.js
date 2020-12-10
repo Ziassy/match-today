f0017922eb0ab41f4ed3e3a2d8c61cecc5d65998
fauziah09 <48745584+fauziah09@users.noreply.github.com>
2020-12-10 08:13:02 +0700

    Submission review revisi

--- js/nav.js
+++ js/nav.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (this.status === 200) {
           content.innerHTML = xhttp.responseText;
-        } else if (this.status == 404) {
+        } else if (this.status === 404) {
           content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
         } else {
           content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
